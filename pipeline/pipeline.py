065e365de733df559a0e36ca2ef18c9fb5e0d6cf
RadoslavKostadinov <147995972+radoslavvkostadinov@users.noreply.github.com>
2026-09-15 08:58:29 +0000

    Pipeline setup created

---
+++ pipeline/pipeline.py
@@ -0,0 +1,11 @@
+import sys
+import pandas as pd
+import pyarrow
+
+
+df = pd.DataFrame({"day":[1,2], "number_passengers":[3,4]})
+month = int(sys.argv[1])
+
+df['month'] = month
+
+df.to_parquet(f"output_{month}.parquet")
